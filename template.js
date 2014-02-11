212d541519cb687ee2d8169675e54785df5791a3
foriacus <634844533@qq.com>
2014-02-11 19:03:18 +0800

    first commit.

---
+++ template.js
@@ -0,0 +1,1 @@
+module.exports = '<div class="nav-paginator" id="page-bottom">\n	<ul class="pager">\n	  <li class="prev"><a href="#"><< 上一页</a></li>\n	  <li class="next"><a href="#">下一页 >></a></li>\n	</ul>\n</div>';
